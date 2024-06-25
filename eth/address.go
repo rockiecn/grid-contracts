d23856d52bb601f24bff0197f1079dcd1bec99f2
王勇 <roc@memoio.io>
2024-06-25 15:39:48 +0800

    fix json path

--- eth/address.go
+++ eth/address.go
@@ -38,7 +38,7 @@ func Save(a Address, pathName string) {
 // load local json
 func LoadJSON() Address {
 	// load file
-	b, err := os.ReadFile("../../grid-contracts/eth/contracts.json")
+	b, err := os.ReadFile("../grid-contracts/eth/contracts.json")
 	if err != nil {
 		fmt.Println("Error reading JSON:", err)
 		return Address{}
